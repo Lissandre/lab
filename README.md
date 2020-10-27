d1b26720b629bbf618ca7adfa48829940abafba8
Lissandre <lissandre.pasdeloup@hetic.net>
2020-10-28 00:05:19 +0100

    webcam done

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A partial list of things I try when I'm bored or when I want to try new things.
 
 ## Experiments
 - [canvas-circle](https://lissandre.fr/lab/canvas-circle) ([source](experiments/canvas-circle/))
+- [square-printing-raster](https://lissandre.fr/lab/square-printing-raster) ([source](experiments/square-printing-raster/))
 
 ## Work in Progress
 - [colored-lines](https://lissandre.fr/lab/colored-lines) ([source](experiments/colored-lines/))
-- [square-printing-raster](https://lissandre.fr/lab/square-printing-raster) ([source](experiments/square-printing-raster/))
--- experiments/square-printing-raster/src/js/script.js
+++ experiments/square-printing-raster/src/js/script.js
@@ -1,60 +1,83 @@
 const canvas = document.querySelector('#_canvas')
+const tempcanvas = document.querySelector('#_tempcanvas')
 /**
  * CANVAS SIZES
  */
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+tempcanvas.width = window.innerWidth
+tempcanvas.height = window.innerHeight
 window.addEventListener('resize', ()=>{
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
+  tempcanvas.width = window.innerWidth
+  tempcanvas.height = window.innerHeight
+  tempctx.clearRect(0,0,tempcanvas.width, tempcanvas.height)
+  // drawImage()
   //draw()
 })
 /**
  * CONTEXT
  */
 const ctx = canvas.getContext('2d')
+const tempctx = tempcanvas.getContext('2d')
 /**
  * IMAGE
  */
-const image = new Image()
-image.src = './image.jpg'
+// const image = new Image()
+// image.src = './image.jpg'
 
-image.onload = () => {
-  print()
-  //let imageLeft = canvas.width/2 - image.width/2
-  //let imageTop = canvas.height/2 - image.height/2
-  //ctx.drawImage(image, imageLeft, imageTop)
-}
+// image.onload = function() {
+//   drawImage()
+//   printDraw()
+//   update()
+// }
 
-function print() {
+// function drawImage(){
+//   tempctx.drawImage(image, 0, 0)
+// }
+// Prefer camera resolution nearest to 1280x720.
+let constraints = { audio: false, video: { width: 1920, height: 1080 } }; 
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  let video = document.querySelector('video')
+  video.srcObject = mediaStream
+  video.onloadedmetadata = function(e) {
+    video.play()
+  }
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end
+function printDraw() {
+  const imageDataSource = tempctx.getImageData(0, 0, video.width, video.height)
   let h = 0
   let w = 0
-  let imageLeft = canvas.width/2 - image.width/2
-  let imageTop = canvas.height/2 - image.height/2
-  ctx.drawImage(image, imageLeft, imageTop)
-  const imageDataSource = ctx.getImageData(imageLeft, imageTop, image.width, image.height)
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.globalCompositeOperation = 'multiply'
-  for (let i = 0; i < imageDataSource.data.length; i+=24) {
-    let rMoyenne = Math.round(((imageDataSource.data[i] + imageDataSource.data[i+4] + imageDataSource.data[i+8] + imageDataSource.data[i+12] + imageDataSource.data[i+16] + imageDataSource.data[i+20])/6)/255)*255
-    let gMoyenne = Math.round(((imageDataSource.data[i+1] + imageDataSource.data[i+5] + imageDataSource.data[i+9] + imageDataSource.data[i+13] + imageDataSource.data[i+17] + imageDataSource.data[i+21])/6)/255)*255
-    let bMoyenne = Math.round(((imageDataSource.data[i+2] + imageDataSource.data[i+6] + imageDataSource.data[i+10] + imageDataSource.data[i+14] + imageDataSource.data[i+18] + imageDataSource.data[i+22])/6)/255)*255
+  // ctx.globalCompositeOperation = 'multiply'
+  for (let i = 0; i < imageDataSource.data.length; i+=44 ){
+    let rMoyenne = Math.round(((imageDataSource.data[i] + imageDataSource.data[i+8] + imageDataSource.data[i+16] + imageDataSource.data[i+24] + imageDataSource.data[i+32] + imageDataSource.data[i+40])/6)/255)*255
+    let gMoyenne = Math.round(((imageDataSource.data[i+1] + imageDataSource.data[i+9] + imageDataSource.data[i+17] + imageDataSource.data[i+25] + imageDataSource.data[i+33] + imageDataSource.data[i+41])/6)/255)*255
+    let bMoyenne = Math.round(((imageDataSource.data[i+2] + imageDataSource.data[i+10] + imageDataSource.data[i+18] + imageDataSource.data[i+26] + imageDataSource.data[i+34] + imageDataSource.data[i+42])/6)/255)*255
 
     ctx.strokeStyle = `rgb(${rMoyenne}, ${gMoyenne}, ${bMoyenne})`
-    ctx.lineWidth = 1
-    //console.log(i%(image.width))
-    if(i%(image.width*4)===4){
+    ctx.lineWidth = 3
+    // console.log((i+43)%(image.width*4))
+    if(((i+43)%(video.width*4))<=43){
       w = 0
       h += 10
+      i += imageDataSource.data.length/video.height*5
     }else{
       w += 10
     }
-    ctx.strokeRect(w, h, 20, 20)
+    ctx.strokeRect(w+Math.round(Math.random()*4-2), h+Math.round(Math.random()*4-2), 20, 20)
   }
 }
 
 const update = function(){
-  window.requestAnimationFrame(update)
+  setTimeout(update, 1000/30)
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+  tempctx.clearRect(0,0,tempcanvas.width, tempcanvas.height)
+  tempctx.drawImage(video, 0, 0, video.width, video.height)
+  printDraw()
 }
 update()
 /**
